f087faf13134b305274c2cdcac74db2d5ce5f135
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2019-10-15 12:13:04 +0100

    feat(simple-http): add corsHandler

--- packages/simple-http-lambda-deploy/src/testLambda.ts
+++ packages/simple-http-lambda-deploy/src/testLambda.ts
@@ -1,13 +1,20 @@
-import { jsonHandler, HttpResponse, HttpRequest } from '@fmtk/simple-http';
+import {
+  jsonHandler,
+  HttpResponse,
+  HttpRequest,
+  corsHandler,
+} from '@fmtk/simple-http';
 import { toAwsLambdaHandler } from '@fmtk/simple-http-lambda';
 
 export const handler = toAwsLambdaHandler(
-  jsonHandler(
-    async (request: HttpRequest<{}>): Promise<HttpResponse<{}>> => {
-      return {
-        status: 200,
-        body: request,
-      };
-    },
+  corsHandler(
+    jsonHandler(
+      async (request: HttpRequest<{}>): Promise<HttpResponse<{}>> => {
+        return {
+          status: 200,
+          body: request,
+        };
+      },
+    ),
   ),
 );
--- packages/simple-http/src/index.ts
+++ packages/simple-http/src/index.ts
@@ -1,3 +1,4 @@
+export * from './corsHandler';
 export * from './HttpContext';
 export * from './HttpHandler';
 export { HttpHeaders } from './HttpHeaders';
