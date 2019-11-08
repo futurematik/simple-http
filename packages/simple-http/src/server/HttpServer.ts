1d828af6f14fa75a78f26224022b4e3ab7de40aa
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2019-11-08 11:43:36 +0000

    loosen HttpServer defaults for contravariance

--- packages/simple-http/src/server/HttpServer.ts
+++ packages/simple-http/src/server/HttpServer.ts
@@ -2,10 +2,10 @@ import { HttpServerRequest } from './HttpServerRequest';
 import { HttpServerResponse } from './HttpServerResponse';
 
 export interface HttpServer<
-  Req = unknown,
+  Req = any, // eslint-disable-line @typescript-eslint/no-explicit-any
   Res = unknown,
-  Ctx = unknown,
-  Query = unknown
+  Ctx = any, // eslint-disable-line @typescript-eslint/no-explicit-any
+  Query = any // eslint-disable-line @typescript-eslint/no-explicit-any
 > {
   (request: HttpServerRequest<Req, Ctx, Query>): PromiseLike<
     HttpServerResponse<Res>
