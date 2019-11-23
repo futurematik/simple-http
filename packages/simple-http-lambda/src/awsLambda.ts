bb0a8c1a010a3a2522585dc1d5bd79f29ccc1452
Gordon Leigh <gordon.leigh@futurematik.co.uk>
2019-11-23 14:16:38 +0000

    feat(simple-http-lambda): add invokeHandlerFromLambda() and convertToLambdaResponse()

--- packages/simple-http-lambda/src/awsLambda.ts
+++ packages/simple-http-lambda/src/awsLambda.ts
@@ -3,6 +3,7 @@ import {
   HttpServerAdapter,
   fromArrayValues,
   toArrayValues,
+  HttpServerResponse,
 } from '@fmtk/simple-http';
 import {
   APIGatewayProxyEvent,
@@ -22,58 +23,17 @@ export type AsyncAPIGatewayProxyHandler = AsyncLambdaHandler<
 >;
 
 export function awsLambda(): HttpServerAdapter<AsyncAPIGatewayProxyHandler> {
-  return (handler: HttpServer): AsyncAPIGatewayProxyHandler =>
-    async function(
+  return (handler: HttpServer): AsyncAPIGatewayProxyHandler => {
+    const invoke = invokeHandlerFromLambda(handler);
+
+    return async function(
       event: APIGatewayProxyEvent,
       context: Context,
     ): Promise<APIGatewayProxyResult> {
-      const requestBody =
-        event.body &&
-        (event.isBase64Encoded
-          ? Buffer.from(event.body, 'base64')
-          : event.body);
-
       try {
-        const response = await handler({
-          body: requestBody || undefined,
-          context: {
-            auth: event.requestContext.authorizer,
-            aws: { event, context },
-            pathParameters: event.pathParameters,
-          },
-          headers: fromArrayValues(event.multiValueHeaders),
-          method: event.httpMethod,
-          path: event.requestContext.path,
-          query: fromArrayValues(event.multiValueQueryStringParameters || {}),
-          resourcePath: event.path,
-        });
-
-        let responseBody: string;
-        let isBase64Encoded: boolean;
-
-        if (response.body === null || typeof response.body === 'undefined') {
-          responseBody = '';
-          isBase64Encoded = false;
-        } else if (typeof response.body === 'string') {
-          responseBody = response.body;
-          isBase64Encoded = false;
-        } else if (Buffer.isBuffer(response.body)) {
-          responseBody = response.body.toString('base64');
-          isBase64Encoded = true;
-        } else {
-          throw new TypeError(
-            `expected response body to be a string or a buffer`,
-          );
-        }
-
-        return {
-          body: responseBody,
-          isBase64Encoded,
-          multiValueHeaders: toArrayValues(response.headers || {}),
-          statusCode: response.status,
-        };
+        const response = await invoke(event, context);
+        return convertToLambdaResponse(response);
       } catch (e) {
-        console.error(e);
         return {
           statusCode: 500,
           body: 'HTTP Server Error 500',
@@ -83,4 +43,56 @@ export function awsLambda(): HttpServerAdapter<AsyncAPIGatewayProxyHandler> {
         };
       }
     };
+  };
+}
+
+export function invokeHandlerFromLambda<Req, Res>(
+  handler: HttpServer<Req, Res>,
+): AsyncLambdaHandler<APIGatewayProxyEvent, HttpServerResponse<Res>> {
+  return async (event, context): Promise<HttpServerResponse<Res>> => {
+    const requestBody =
+      event.body &&
+      (event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body);
+
+    return await handler({
+      body: ((requestBody || undefined) as unknown) as Req,
+      context: {
+        auth: event.requestContext.authorizer,
+        aws: { event, context },
+        pathParameters: event.pathParameters,
+      },
+      headers: fromArrayValues(event.multiValueHeaders),
+      method: event.httpMethod,
+      path: event.requestContext.path,
+      query: fromArrayValues(event.multiValueQueryStringParameters || {}),
+      resourcePath: event.path,
+    });
+  };
+}
+
+export function convertToLambdaResponse(
+  response: HttpServerResponse,
+): APIGatewayProxyResult {
+  let responseBody: string;
+  let isBase64Encoded: boolean;
+
+  if (response.body === null || typeof response.body === 'undefined') {
+    responseBody = '';
+    isBase64Encoded = false;
+  } else if (typeof response.body === 'string') {
+    responseBody = response.body;
+    isBase64Encoded = false;
+  } else if (Buffer.isBuffer(response.body)) {
+    responseBody = response.body.toString('base64');
+    isBase64Encoded = true;
+  } else {
+    throw new TypeError(`expected response body to be a string or a buffer`);
+  }
+
+  return {
+    body: responseBody,
+    isBase64Encoded,
+    multiValueHeaders: toArrayValues(response.headers || {}),
+    statusCode: response.status,
+  };
 }
